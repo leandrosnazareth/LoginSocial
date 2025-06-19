2d2295326a57352c76eb5781d42a5fa4ebd05462
Leandro Nazareth <leandrosnazareth@gmail.com>
2025-06-19 10:21:07 -0300

    protegendo as keys no env

--- src/main/java/com/leandrosnazareth/LoginSocial/LoginSocialApplication.java
+++ src/main/java/com/leandrosnazareth/LoginSocial/LoginSocialApplication.java
@@ -3,10 +3,19 @@ package com.leandrosnazareth.LoginSocial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LoginSocialApplication {
 
 	public static void main(String[] args) {
+		// Carrega o .env
+		Dotenv dotenv = Dotenv.load();
+
+		// Passa as variáveis para o System properties
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(LoginSocialApplication.class, args);
 	}
 
